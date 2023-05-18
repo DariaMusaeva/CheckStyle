b7f57e0e8d114a9fd662fa9da492ef0a2f42b9f4
Eldar Musaev <mel57792@yandex.ru>
2023-05-18 18:51:09 +0500

    new Test created

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,17 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxIfNotFirst() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {11, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
